9220bf14e4ae49d9f346728d4e3bf220cbe3ca25
Iryna Sysiak <irinka1369@gmail.com>
2019-12-24 16:53:37 +0200

    fixed bugs according to eslint

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,20 +1,22 @@
-const a = +prompt("Please enter number");
-const b = +prompt("Please enter number");
-const c = +prompt("Please enter number");
-const discr = b * b - 4 * a * c;
+const a = +prompt('Please enter number');
+const b = +prompt('Please enter number');
+const c = +prompt('Please enter number');
+const numberFour = 4;
+const numberTwo = 2;
+const discr = b * b - numberFour * a * c;
 const discrSqrt = Math.sqrt(discr);
 
 if (isNaN(a) || isNaN(b) || isNaN(c) || a === 0 || b === 0 || c === 0 ) {
-  alert("You have entered invalid data");
+  alert('You have entered invalid data');
 } else {
   if (discr < 0) {
-    console.log("Sorry, no solution");
+    console.log('Sorry, no solution');
   } else if (discr === 0) {
-    let x = Math.round(-b / (2 * a));
+    let x = Math.round(-b / (numberTwo * a));
     console.log(`x = ${x}`);
   } else {
-    let x1 = Math.round(-b - discrSqrt / (2 * a));
-    let x2 = Math.round(-b + discrSqrt / (2 * a));
+    let x1 = Math.round(-b - discrSqrt / (numberTwo * a));
+    let x2 = Math.round(-b + discrSqrt / (numberTwo * a));
     console.log(`x1 = ${x1} and x2 = ${x2}`);
   }
 }
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,17 +1,17 @@
-const a = +prompt("Please enter triangle side length");
-const b = +prompt("Please enter triangle side length");
-const c = +prompt("Please enter triangle side length");
+const a = +prompt('Please enter triangle side length');
+const b = +prompt('Please enter triangle side length');
+const c = +prompt('Please enter triangle side length');
 
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
-  alert("Input values should be ONLY numbers ");
+  alert('Input values should be ONLY numbers ');
 } else if (a === 0 || b === 0 || c === 0) {
-  alert("A triangle must have 3 sides with a positive definite length ");
+  alert('Our triangle must have 3 sides with a positive definite length.');
 } else if (!(a + b > c && a + c > b && b + c > a)) {
-  console.log("Triangle doesn’t exist");
+  console.log('Triangle doesn’t exist');
 } else if (a === b && b === c && a === c) {
-  console.log(" It is an equilateral triangle");
+  console.log('It is an equilateral triangle');
 } else if (a === b || b === c || a === c) {
-  console.log(" It is an isosceles triangle");
+  console.log('It is an isosceles triangle');
 } else {
-  console.log(" It is a scalene triangle");
+  console.log('It is a scalene triangle');
 }
